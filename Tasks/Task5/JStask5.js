beebf2798c92b2284571af5773aed455771bc9c3
sathwik-reddy49 <indurusathwikreddy049@gmail.com>
2024-12-11 00:17:59 +0530

    Tasks resub

---
+++ Tasks/Task5/JStask5.js
@@ -0,0 +1,36 @@
+function a(){
+    var obj = {
+        hero_name : "Allu Arjun",
+        movie_name : "Pushpa 2",
+        director_name : "Sukhumar",
+        plotof_movie : "Action"
+    }
+    console.log(obj);
+}
+a();
+
+// var a = 1;
+// while(a<=10){
+//     console.log(a);
+//     a++;
+// }
+
+// var a = 10;
+// while(a>0){
+//     console.log(a);
+//     a--;    
+// }
+
+// var a = -1;
+// while(a>=-10){
+//     console.log(a);
+//     a--;
+// }
+
+// var a = -10;
+// while(a<0){
+//     console.log(a);
+//     a++;
+// }
+
+var a = prompt("Please enter a number");
